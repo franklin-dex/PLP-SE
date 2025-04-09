e09ccc771be7f9422e2a07f009e2e18a7499c267
franklin-dex <odinakachukwufranklin0@gmail.com>
2025-04-09 17:00:09 +0100

    wk3 assignment

---
+++ wk3_python_functions/Answer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/
+def calculate_discount(price, discount_percent):
+    """
+    Calculate the final price after applying a discount if the discount is 20% or higher.
+    
+    Parameters:
+    price (float): The original price of the item.
+    discount_percent (float): The discount percentage to be applied.
+    
+    Returns:
+    float: The final price after applying the discount (if applicable), otherwise the original price.
+    """
+    if discount_percent >= 20:
+        discount_amount = price * (discount_percent / 100)
+        final_price = price - discount_amount
+        return final_price
+    else:
+        return price
+    
+def main():
+    try:
+        price = float(input("Enter the original price: "))
+        discount_percent = float(input("Enter the discount percent: "))
+        
+        final_price = discount_percent(price, discount_percent)
+        
+        if discount_percent >= 20:
+            print(f"Final price after {discount_percent}% discount is: ${final_price}")
+            
+        else:
+            print(f"No discount applied. original price: ${final_price}")
+            
+    except ValueError:
+        print("Enter a valid number")
+
+if __name__ == "__main__":
+    main()    
